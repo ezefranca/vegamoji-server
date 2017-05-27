3c4d30f807293459cde2d9cae1bcb8d6c1427266
Ezequiel <ezequiel.santos@btgpactual.com>
2017-05-27 13:02:39 -0300

    test routes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 //https://developer.edamam.com/edamam-docs-recipe-api
-$.get('/edamam/search?q=vegan')
+$(function() {
+  console.log('123 testando... :o');
+
+  $.get('/edamam/search?q=vegetarian')
 .then(data => {
   console.log(data.hits);
   data.hits.forEach(nom => $('#results').append(`<p>${nom.recipe.label}<img src="${nom.recipe.image}" alt="${nom.recipe.label}" height="42" width="42"></p>`));
@@ -7,3 +10,19 @@ $.get('/edamam/search?q=vegan')
   err => {
     console.error('Status Code: ', err.status);
   });
+  
+  $.get('/salads', function(salads) {
+    salads.forEach(function(result) {
+      $('<li></li>').text(result).appendTo('ul#salads');
+    });
+  });
+
+  $.get('/edamam', function(res) {
+    res.forEach(function(result) {
+      $('<li></li>').text(result).appendTo('TESTE');
+    });
+  });
+
+});
+
+
--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const express = require('express');
 const requestProxy = require('express-request-proxy');
+var emojiText = require("emoji-text");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+    res.sendFile('index.html')
+  	console.log(request.query.receitas);
+    response.send('Response receitas:' + request.query.receitas);
+    
+    (requestProxy({
+    url: `https://api.edamam.com/search`,
+    headers: {
+      app_id: '551d4b2e', // ID
+      app_key: 'e7e1c5904bbd8ea64e41e983e4702632'}  // API KEY
+  }))(request, response);
 });
 
+app.get("/salads", function (request, response) {
+  response.send(salads);
+});
+
+app.get('/receitas', function (request, response) {
+	
+	console.log(request.query.receitas);
+    response.send('Response receitas:' + request.query.receitas);
+    
+    (requestProxy({
+    url: `https://api.edamam.com/search`,
+    headers: {
+      app_id: '551d4b2e', // ID
+      app_key: 'e7e1c5904bbd8ea64e41e983e4702632'}  // API KEY
+  }))(request, response);
+});
+
+var salads = [
+
+  "salads one",
+  "salads two",
+  "salads three"
+
+];
+
 // handles the route for our AJAX request to Edamam
-app.get('/edamam/*', proxyEdamam);
+app.get('/edamam*/', proxyEdamam);
+
 
 function proxyEdamam(request, response) {
+
   console.log('Routing Edamam request');
   console.log('Deu certo');
+  console.log(emojiText.convert("🍎"));
   (requestProxy({
     url: `https://api.edamam.com/search`,
     headers: {
